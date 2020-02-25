f5c8e7f73ebd2c8638f4e3e8840b93b1b72d1af7
panrusheng <panrusheng@zju.edu.cn>
2020-02-25 16:50:50 +0800

    update layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import './App.css'
-import graphData from './data/player_graph/1/1_friend' 
-import NodeLink from './com/nodeLink'
-import Projection from './com/projection'
-import Charts from './com/charts'
+import React from "react";
+import "./App.css";
+import graphData from "./data/constructed_graph_add_topology_attributes/42/42_trade_undirected_topo_attrs";
+import projectionData from "./data/embedding_data/42_trade_embedding";
+import NodeLink from "./com/nodeLink";
+import Projection from "./com/projection";
+import Charts from "./com/charts";
 function App() {
-	return (
-		<div
-			style={{
-				padding: '20px',
-				display: 'grid',
-				gridTemplateColumns: 'auto auto',
-				gridTemplateRows: 'auto',
-				gridGap: '10px',
-			}}
-		>
-			<div>
-				<Charts graphData = {graphData} />
-			</div>
-			<Projection />
-			<div style={{ height: 1080, width: 920 }}>
-				<NodeLink graphData = {graphData}/>
-			</div>
-		</div>
-	)
+  return (
+    <div
+      style={{
+        padding: "20px",
+        display: "grid",
+        gridTemplateColumns: "1fr 3fr",
+        gridTemplateRows: "1fr 1fr",
+        gridGap: "10px",
+        placeItems: "center center",
+        gridAutoFlow: "column"
+      }}
+    >
+      <div>
+        <Projection projectionData={projectionData} />
+      </div>
+      <div>
+        <Charts graphData={graphData} />
+      </div>
+      <div style={{ gridRowStart: "span 2" }}>
+        <NodeLink graphData={graphData} />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/com/projection.js
+++ src/com/projection.js
@@ -1,85 +1,89 @@
-import React from 'react'
-import { G } from './G.js'
-import { Switch } from 'antd'
-import { ProjectionData } from '../data/prjectionData'
-import { nodeStyle } from '../style/nodeLinkStyle'
+import React from "react";
+import { G } from "./G.js";
+import { Switch } from "antd";
+import { ProjectionData } from "../data/prjectionData";
+import { nodeStyle } from "../style/nodeLinkStyle";
 
 class Projection extends React.Component {
-	constructor(props) {
-		super(props)
-		this.lassoNdoes = []
-		this.g = new G({
-			data: ProjectionData,
-		})
-		this.nodeRefresh = (width, height) => {
-			this.g.beginBatch()
-			this.g.nodes().forEach((node, i) => {
-				node.fill = nodeStyle.fill
-				node.strokeWidth = nodeStyle.strokeWidth
-				node.renderID = i
-				node.r = nodeStyle.r
-				node.strokeColor = nodeStyle.strokeColor
-				node.x = Math.random() * width
-				node.y = Math.random() * height
-			})
-			this.g.endBatch()
-			this.g.refresh()
-		}
-		this.switchOnchange = (checked) => {
-			if (checked) {
-				this.g.toggleLasso(true)
-			} else {
-				this.g.toggleLasso(false)
-			}
-		}
-	}
-	render() {
-		return (
-			<div id="projection" style={{ position: 'relative' }}>
-				<canvas
-					id="projection-canvas"
-					style={{ position: 'absolute' }}
-					width="300"
-					height="300"
-				></canvas>
-				<Switch
-					checkedChildren="ON"
-					unCheckedChildren="OFF"
-					defaultChecked
-					onChange={this.switchOnchange}
-				/>
-			</div>
-		)
-	}
-	componentDidMount() {
-		const canvas = document.getElementById('projection-canvas')
-		const width = canvas.width
-		const height = canvas.height
-		const g = this.g
-		g.container(canvas)
-		this.nodeRefresh(width, height)
-		g.initLasso(document.querySelector('#projection'))
-		g.on('zoom', () => {})
-		g.on('pan', () => {})
-		g.on('lasso', (nodes) => {
-			g.beginBatch()
-			this.lassoNdoes.forEach((n) => {
-				n.fill = nodeStyle.fill
-				n.r = nodeStyle.r
-			})
-			this.g.nodes().forEach((node) => {
-				node.fill = nodeStyle.fill
-				node.r = nodeStyle.r
-			})
-			console.log(nodes)
-			this.lassoNdoes = nodes
-			nodes.forEach((n) => {
-				n.fill = nodeStyle.lassoFill
-				n.r = nodeStyle.lassoR
-			})
-			g.endBatch()
-			g.refresh()
-		})
-	}
+  constructor(props) {
+    super(props);
+    // let ProjectionData = props.projectionData;
+    this.lassoNdoes = [];
+    this.g = new G({
+      data: ProjectionData
+    });
+    this.nodeRefresh = (width, height) => {
+      this.g.beginBatch();
+      this.g.nodes().forEach((node, i) => {
+        node.fill = nodeStyle.fill;
+        node.strokeWidth = nodeStyle.strokeWidth;
+        node.renderID = i;
+        node.r = nodeStyle.r;
+        node.strokeColor = nodeStyle.strokeColor;
+        node.x = Math.random() * width;
+        node.y = Math.random() * height;
+      });
+      this.g.endBatch();
+      this.g.refresh();
+    };
+    this.switchOnchange = checked => {
+      if (checked) {
+        this.g.toggleLasso(true);
+      } else {
+        this.g.toggleLasso(false);
+      }
+    };
+  }
+  render() {
+    return (
+      <div
+        id="projection"
+        style={{ position: "relative", width: "300px", height: "300px" }}
+      >
+        <canvas
+          id="projection-canvas"
+          style={{ position: "absolute" }}
+          width="300"
+          height="300"
+        ></canvas>
+        <Switch
+          checkedChildren="ON"
+          unCheckedChildren="OFF"
+          defaultChecked
+          onChange={this.switchOnchange}
+        />
+      </div>
+    );
+  }
+  componentDidMount() {
+    const canvas = document.getElementById("projection-canvas");
+    const width = canvas.width;
+    const height = canvas.height;
+    const g = this.g;
+    g.container(canvas);
+    this.nodeRefresh(width, height);
+    g.initLasso(document.querySelector("#projection"));
+    g.on("zoom", () => {});
+    g.on("pan", () => {});
+    g.on("lasso", nodes => {
+      g.beginBatch();
+      this.lassoNdoes.forEach(n => {
+        n.fill = nodeStyle.fill;
+        n.r = nodeStyle.r;
+      });
+      this.g.nodes().forEach(node => {
+        node.fill = nodeStyle.fill;
+        node.r = nodeStyle.r;
+      });
+      console.log(nodes);
+      this.lassoNdoes = nodes;
+      nodes.forEach(n => {
+        n.fill = nodeStyle.lassoFill;
+        n.r = nodeStyle.lassoR;
+      });
+      g.endBatch();
+      g.refresh();
+    });
+  }
 }
-export default Projection
+export default Projection;
